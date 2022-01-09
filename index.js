fa32ccc0750c493741db9f8fd73bd94f7e6fb091
Michael <MichaelCheung101@gmail.com>
2022-01-08 23:19:03 -0500

    commit tree c74ac7a9dc0001ec05c99250b47aed995c6d9c26

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+
+  if(distance <= 400){
+    return('This one is on me!')
+      } else if(distance > 2500){
+      return('No can do.')
+    }else {
+      return('I will gladly take your thirty bucks.')
+    }
+  }
+
+
+function ternaryCheckCity(city){
+
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.'
+    break;
+    case 'not as generous':
+    return 'Thank you.'
+    default:
+      return('Bye.')
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
 }
